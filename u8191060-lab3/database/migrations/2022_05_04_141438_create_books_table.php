e6bf36716d73781bab3c86f0b80ce14415bfed88
C-4KE <glebkinkirill@gmail.com>
2022-05-04 18:26:32 +0300

    Добавлены миграции для таблиц.

---
+++ u8191060-lab3/database/migrations/2022_05_04_141438_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('publisher');
+            $table->date('release_date');
+            $table->unsignedBigInteger('bookcase_id');
+            $table->timestamps();
+            
+            $table->foreign('bookcase_id')->references('id')->on('bookcases');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
